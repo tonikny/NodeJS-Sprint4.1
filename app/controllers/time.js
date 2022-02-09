9869f9524fd6e86e580def65b2b74a6471b93a00
tonikny <dalecanya@gmail.com>
2022-02-09 12:11:54 +0100

    Nivell 2

--- app/controllers/time.js
+++ app/controllers/time.js
@@ -1,7 +1,12 @@
 
 const time = (req, res) => {
-  const now = Date.now();
-  res.send(now);
+  if (req.body.user) {
+    console.log("Petició de la data i hora per l'usuari: ", req.body.user);
+    const now = Date();
+    res.json({ now });
+  } else {
+    res.send('No hi ha usuari a la peticio');
+  }
 }
 
-module.exports = { time }
+module.exports = time;
--- app/routes/router.js
+++ app/routes/router.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+
 const rutaUser = require('../controllers/user');
-const { uploadSingle, uploadImatge } = require('../controllers/upload');
-
-
 router.get('/user', rutaUser.user);
 
+
+const { uploadSingle, uploadImatge } = require('../controllers/upload');
 router.post('/upload', uploadSingle, uploadImatge);
 
-router.post('/time', '');
+
+const cors = require('cors');
+const noCache = require('../middlewares/noCache');
+const time = require('../controllers/time');
+router.post(
+  '/time',          // ruta
+  cors(),           // cors (express)
+  noCache,          // app/middlewares/noCache.js
+  express.json(),   // per acceptar paràmetre json
+  time              // retorna data/hora si hi ha parámetre usuari
+);
 
 
 module.exports = { router }
